c9c5cba98a2416106d2fc87c227ce887b36b1f68
taisy9170@yahoo.co.jp <d-inoue@wwwave.jp>
2016-02-29 13:46:42 +0900

    Example Code

--- index.ios.js
+++ index.ios.js
@@ -1,6 +1,8 @@
 /**
  * Sample React Native App
  * https://github.com/facebook/react-native
+ * A sound file is from url.
+ * http://www.noiseaddicts.com/free-samples-mp3/?id=4933
  */
 'use strict';
 import React, {
@@ -8,10 +10,39 @@ import React, {
   Component,
   StyleSheet,
   Text,
-  View
+  View,
+  TouchableHighlight
 } from 'react-native';
 
+var Sound = require('react-native-sound');
+
 class sound_test extends Component {
+  constructor() {
+    super();
+    /* Bind Member function for using 'this'. */
+    this._onPressPlay   = this._onPressPlay.bind(this);
+    this._onPressStop   = this._onPressStop.bind(this);
+    this._onPressResume = this._onPressResume.bind(this);
+    this._onPressPause  = this._onPressPause.bind(this);
+
+    this.whoosh = new Sound('whoosh.mp3',
+                            /* your path here */
+                            Sound.DOCUMENT,
+                           /* Exit Event */
+                           (error) => {
+
+                             if( error ) {
+                               console.log('failed to load the sound',error);
+                             } else {
+                               console.log('duration in seconds: ' + this.whoosh.duration +
+                                           'number of channels: ' + this.whoosh.numberOfChannels );
+                             }
+                           });
+    console.log('MAIN_BUNDLE: ',Sound.MAIN_BUNDLE);
+    console.log('DOCUMENT:'    ,Sound.DOCUMENT);
+    console.log('LIBRARY:'     ,Sound.LIBRARY);
+    console.log('CACHES:'      ,Sound.CACHES);
+  }
   render() {
     return (
       <View style={styles.container}>
@@ -24,9 +55,75 @@ class sound_test extends Component {
         <Text style={styles.instructions}>
           Press Cmd+R to reload,{'\n'}
           Cmd+D or shake for dev menu
-        </Text>
+      </Text>
+      <TouchableHighlight onPress={this._onPressPlay}>
+        <View style={styles.button}>
+          <Text>Play</Text>
+        </View>
+      </TouchableHighlight>
+      <TouchableHighlight onPress={this._onPressStop}>
+      <View style={styles.button}>
+      <Text>Stop</Text>
+      </View>
+      </TouchableHighlight>
+
+      <TouchableHighlight onPress={this._onPressResume}>
+      <View style={styles.button}>
+      <Text>Resume</Text>
+      </View>
+      </TouchableHighlight>
+
+      <TouchableHighlight onPress={this._onPressPause}>
+      <View style={styles.button}>
+      <Text>Pause</Text>
+      </View>
+      </TouchableHighlight>
+
       </View>
     );
+  }
+
+  _onPressPlay() {
+    console.log('OnPlay');
+
+    /*     whoosh.setVolume(0.5); */
+    this.whoosh.setPan(1);
+    this.whoosh.setNumberOfLoops(0);
+
+    console.log('volume: ' + this.whoosh.getVolume());
+    console.log('pan: '    + this.whoosh.getPan());
+    console.log('loops: '  + this.whoosh.getNumberOfLoops());
+
+    this.whoosh.setCurrentTime(0);
+
+    this.whoosh.play((success)=>{
+      if(success) {
+        console.log('successfully finished playing');
+      }  else {
+        console.log('playback failed due to audio decoding errors');
+      }
+    });
+  }
+
+  _onPressStop() {
+    console.log('OnStop');
+    this.whoosh.stop();
+  }
+
+  _onPressResume() {
+    console.log('OnResume');
+    this.whoosh.play((success)=>{
+      if(success) {
+        console.log('successfully finished playing');
+      }  else {
+        console.log('playback failed due to audio decoding errors');
+      }
+    });
+  }
+
+  _onPressPause() {
+    console.log('OnPause');
+    this.whoosh.pause();
   }
 }
 
@@ -46,6 +143,10 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     color: '#333333',
     marginBottom: 5,
+  },
+  button: {
+    margin: 20,
+    backgroundColor: 'rgba(52,52,52,0.5)'
   },
 });
 
